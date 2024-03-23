7278c6d5bc2af1fe49314d80e4dee846f4e83fe7
hossein <hosseinfahimi66@gmail.com>
2024-03-23 22:42:04 +0330

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Cards';
+posts=[
+  {
+      imageurl:'../assets/Images/omen-1.jpg',
+      title:'test1',
+      content:'Some quick example text to build on the card title and make up the bulk of the card',
+      btnName:'btn1'
+  },
+  {
+    imageurl:'../assets/Images/omen.jpg',
+    title:'test2',
+    content:'Some quick example text to build on the card title and make up the bulk of the card',
+    btnName:'btn2'
+  },
+  {
+    imageurl:'../assets/Images/OMEN.png',
+    title:'test3',
+    content:'Some quick example text to build on the card title and make up the bulk of the card',
+    btnName:'btn3'
+  }
+]
+getposts(){
+  return this.posts;
+}
 }
